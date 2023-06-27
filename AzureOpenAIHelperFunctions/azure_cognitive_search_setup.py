52b51e3daad3b02b47cbde426a702fbf9f7be642
Tirthankar Barari <tirtho@hotmail.com>
2023-06-27 17:48:57 -0400

    Added code to read csv docs, generate vectors, store in azure cognitive search and then search in the notebook. Also, created helper functions for azure cognitive search.

--- AzureOpenAIHelperFunctions/azure_cognitive_search_setup.py
+++ AzureOpenAIHelperFunctions/azure_cognitive_search_setup.py
@@ -7,6 +7,81 @@ COGNITIVE_SEARCH_ENDPOINT_VALUE = ''
 COGNITIVE_SEARCH_KEY_VALUE = ''
 COGNITIVE_SEARCH_INDEX_VALUE = ''
 
+def create_cognitive_search_index(index_name, search_index_client):
+    from azure.core.credentials import AzureKeyCredential  
+    from azure.search.documents import SearchClient  
+    from azure.search.documents.indexes import SearchIndexClient  
+    from azure.search.documents.models import Vector  
+    from azure.search.documents.indexes.models import (  
+        SearchIndex,  
+        SearchField,  
+        SearchFieldDataType,  
+        SimpleField,  
+        SearchableField,  
+        SearchIndex,  
+        SemanticConfiguration,  
+        PrioritizedFields,  
+        SemanticField,  
+        SearchField,  
+        SemanticSettings,  
+        VectorSearch,  
+        VectorSearchAlgorithmConfiguration,  
+    )  
+    VECTOR_SEARCH_CONFIG_NAME = index_name + "-vector-config"
+    SEMANTIC_SEARCH_CONFIG_NAME = index_name + "-semantic-config"
+
+    fields = [
+        SimpleField(name="id", type=SearchFieldDataType.String, 
+                        filterable=True, key=True),
+        SearchableField(name="title", type=SearchFieldDataType.String,
+                        searchable=True),
+        SearchableField(name="content", type=SearchFieldDataType.String,
+                        searchable=True),
+        SearchableField(name="tag", type=SearchFieldDataType.String,
+                        filterable=True, searchable=True),
+        SearchableField(name="metadata", type=SearchFieldDataType.String,
+                        filterable=True, searchable=True),
+        SearchField(name="content_vector", type=SearchFieldDataType.Collection(SearchFieldDataType.Single),
+                    searchable=True,
+                    dimensions=1536, vector_search_configuration=VECTOR_SEARCH_CONFIG_NAME)
+    ]
+
+    vector_search_config = VectorSearch(
+        algorithm_configurations=[
+            VectorSearchAlgorithmConfiguration(
+                name=VECTOR_SEARCH_CONFIG_NAME,
+                kind="hnsw",
+                hnsw_parameters={
+                    "m": 4,
+                    "efConstruction": 400,
+                    "metric": "cosine"
+                }
+            )
+        ]
+    )
+
+    semantic_search_config = SemanticConfiguration(
+        name=SEMANTIC_SEARCH_CONFIG_NAME,
+        prioritized_fields=PrioritizedFields(
+            title_field=SemanticField(field_name="title"),
+            prioritized_keywords_fields=[SemanticField(field_name="tag")],
+            prioritized_content_fields=[SemanticField(field_name="content")]
+        )
+    )
+
+    # Create the semantic settings with the configuration
+    semantic_settings = SemanticSettings(configurations=[semantic_search_config])
+
+    # Create the search index with the semantic settings
+    index = SearchIndex(name=index_name, fields=fields,
+                        vector_search=vector_search_config, semantic_settings=semantic_settings)
+
+    # Delete index if it exists, to do a clean start
+    search_index_client.delete_index(index_name)
+    # Add the index
+    result = search_index_client.create_or_update_index(index)
+    return result
+
 def set_cognitive_search_config():
     #KeysFromEnv, KeysFromAKVWithMI, KeysFromAKVWithCLIAuth
     match os.getenv('OPENAI_AUTH_TYPE'):
